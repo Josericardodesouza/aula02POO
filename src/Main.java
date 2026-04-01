773d0df4591cf8566efbe395bc30d932e498d2a5
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2026-04-01 00:11:39 -0300

    final de aula

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        int teste;
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+
+       // c1.tampada = true; //chamada atributo
+        //c1.status(); // referencia metodo
+
+        c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c1.modelo = "Hostnet";
+        c1.cor = "Preta";
+        c2.destampar();
+        c2.rabiscar();
+
+
+
+
+
+
 
 
     }
